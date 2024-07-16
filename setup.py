01289e91d18f37c349fecff6869bd2134018703f
Renn F <rennf93@gmail.com>
2024-07-16 15:05:26 +0200

    Fixing versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='fast-man',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     install_requires=[
         'fastapi',
