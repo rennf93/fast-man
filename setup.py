fd6fe4587bae095eb8ea289b3e238b6986806dc4
Renn F <rennf93@gmail.com>
2024-07-26 01:59:33 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -6,9 +6,13 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="fast-man",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
-    install_requires=["fastapi", "pydantic", "requests"],
+    install_requires=[
+        "fastapi",
+        "pydantic",
+        "requests"
+    ],
     entry_points={
         "console_scripts": [
             "fast-man=fast_man.converter:main",
