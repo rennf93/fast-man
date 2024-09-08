3aadac1fdd07df5f4e5243339b0a2b59b791929e
Renn F <rennf93@gmail.com>
2024-09-08 15:36:02 +0200

    version upgrade

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="fast-man",
-    version="0.1.8",
+    version="0.1.9",
     packages=find_packages(),
     install_requires=[
         "fastapi",
